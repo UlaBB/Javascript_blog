f66b3f31e297a3be76917c224731b5d68f7f60f5
Ula Buczak <mankowskau@gmail.com>
2020-09-10 00:15:06 +0200

    Update project

--- js/script.js
+++ js/script.js
@@ -1,75 +1,8 @@
 
-
-// function titleClickHandler() {
-//     const links = document.querySelectorAll('.titles a');
-//     console.log(links);
-// }
-
-// const buttonTest = document.getElementById('button-test');
-
-// buttonTest.addEventListener('click', titleClickHandler);
-
-
-
-// function titleClickHandler() {
-//     console.log('Link was clicked!');
-// }
-
-// const links = document.querySelectorAll('.titles a');
-
-// for (let link of links) {
-//     link.addEventListener('click', titleClickHandler);// funkcj nasluchujacej mozemy uzyc tylko dla poj. elemencie dlatego dla wielu ele. nalezy dac petle
-// }
-
-
-
-
-// 'use strict';
-
-// function titleClickHandler(event) {
-//     console.log('Link was clicked!');
-
-//     /* [DONE] remove class 'active' from all article links  */
-//     const activeLinks = document.querySelectorAll('.titles a.active');
-//     for (let activeLink of activeLinks) {
-//         activeLink.classList.remove('active');
-//     }
-//     console.log(activeLinks);
-
-//     /* [IN PROGRESS] add class 'active' to the clicked link */
-//     console.log('clickedElement:', clickedElement);
-//     console.log('clickedElement (with plus): ' + clickedElement);
-
-
-//     /* [DONE] remove class 'active' from all articles */
-//     const activeArticles = document.querySelectorAll('.posts article.active');
-//     for (let activeArticle of activeArticles) {
-//         activeArticle.classList.remove('active');
-//     }
-
-
-
-//     /* get 'href' attribute from the clicked link */
-
-
-
-//     /* find the correct article using the selector (value of 'href' attribute) */
-
-//     /* add class 'active' to the correct article */
-// }
-
-// const links = document.querySelectorAll('.titles a');
-
-// for (let link of links) {
-//     link.addEventListener('click', titleClickHandler);
-// }
-
-
-
 'use strict';
 
-function titleClickHandler(event) {// od zdarzenia, od kliku!
-    event.preventDefault();// usuwa w adresie strony hash z linku
+function titleClickHandler(event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -109,7 +42,57 @@ function titleClickHandler(event) {// od zdarzenia, od kliku!
     addClassArticle();
 }
 
-const links = document.querySelectorAll('.titles a'); // wybieramy wszystkie linki a w klasie title
-for (let link of links) {
-    link.addEventListener('click', titleClickHandler); // ponieważ "nasluchiwacz" dziala tylko na jeden ele. robimy petle
-}
+// const links = document.querySelectorAll('.titles a'); 
+// for (let link of links) {
+//     link.addEventListener('click', titleClickHandler); 
+// }
+// console.log(links);
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);//wybiera mi wszystkie artyuklu z klasa "post"
+    let html = '';
+    for (let article of articles) {
+        /* get the article id */
+        const articleID = article.getAttribute('id');
+        console.log(articleID);
+
+
+        /* find the title element */
+
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;// zdobycie tytulu artykulu
+        console.log('Title of article', articleTitle);
+
+
+        /* get the title from the title element */
+
+        const linkHTML = '<li><a href="#' + articleID + '"><span>' + articleTitle + '</span></a></li>';
+        // console.log(linkHTML);
+
+        /* create HTML of the link */
+        // titleList.innerHTML = titleList.innerHTML + linkHTML;
+        // titleList.insertAdjacentHTML('beforeend', linkHTML);// pierwszy sposob
+
+        /* insert link into titleList */
+
+        /*insert link into hmtl variable */
+        html = html + linkHTML;
+    };
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+    for (let link of links) {
+        link.addEventListener('click', titleClickHandler);
+    }
+    console.log(links);
+};
+
+generateTitleLinks();
