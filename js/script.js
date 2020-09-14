1de4af5e77aedf86943aa562090a971272db1271
Ula Buczak <mankowskau@gmail.com>
2020-09-14 23:01:03 +0200

    Update function generate tags

--- js/script.js
+++ js/script.js
@@ -54,7 +54,11 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optAuthorsSelector = '.post-author';
+  optAuthorsSelector = '.post-author',
+  optTagsListSelector = '.tags .list',
+  optCloudClassCount = '5',
+  optCloudClassPrefix = 'tag-size-';
+
 
 function generateTitleLinks(customSelector = '') {
   // console.log(customSelector);
@@ -97,9 +101,38 @@ function generateTitleLinks(customSelector = '') {
 }
 generateTitleLinks();
 
+
+function calculateTagsParams(tags) {
+  const params = { max: '0', min: '999999' };
+  // argumentem będzie all tags, kluczami tagi
+  for (let tag in tags) {
+    console.log(tag + ' is used ' + tags[tag] + ' times');
+
+    if (tags[tag] > params.max) {
+      params.max = tags[tag];
+    }
+    if (tags[tag] < params.min) {
+      params.min = tags[tag];
+    }
+  }
+  return params;
+}
+
+function calculateTagClass(count, params) {
+  const normalizedCount = count - params.min;
+  const normalizedMax = params.max - params.min;
+  const percentage = normalizedCount / normalizedMax;
+  const classNumber = Math.floor(percentage * (optCloudClassCount - 1) + 1);
+
+  return optCloudClassPrefix + classNumber;
+}
+
 function generateTags() {
+  /* [NEW] create a new variable allTags with an empty object */
+  let allTags = {};
+
   /* find all articles */
-  const articles = document.querySelectorAll(optArticleSelector);// wyszukuje wszystkie artykuly
+  const articles = document.querySelectorAll(optArticleSelector);
 
   /* START LOOP: for every article: */
   for (let article of articles) {
@@ -111,11 +144,11 @@ function generateTags() {
     let html = '';
 
     /* get tags from data-tags attribute */
-    let articleTags = article.getAttribute('data-tags');
+    let articleTags = article.getAttribute('data-tags');// pobieramy wszystkie atrybuty data-tags
     // console.log(articleTags);
 
     /* split tags into array */
-    const articleTagsArray = articleTags.split(' ');
+    const articleTagsArray = articleTags.split(' '); //dzieli tekst na elementy tablicy
     // console.log(articleTagsArray);
 
     /* START LOOP: for each tag */
@@ -123,11 +156,19 @@ function generateTags() {
 
       /* generate HTML of the link */
       const tagHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
-      //<li><a href="#tag-ziemniaki">ziemniaki</a></li>
 
       /* add generated code to html variable */
       html = html + tagHTML;
       // console.log('Tag html:' + html);
+
+      /* [NEW] check if this link is NOT already in allTags */
+      if (!allTags.hasOwnProperty(tag)) { // 
+        /* [NEW] add generated code to allTags array */
+        allTags[tag] = 1;
+      }
+      else {
+        allTags[tag]++;
+      }
 
       /* END LOOP: for each tag */
     }
@@ -137,8 +178,39 @@ function generateTags() {
 
     /* END LOOP: for every article: */
   }
-}
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
+  console.log(tagList);
 
+  const tagsParams = calculateTagsParams(allTags);
+  console.log('tagsParams:', tagsParams);
+
+  /* [NEW] add html from allTags to tagList */
+  // tagList.innerHTML = allTags.join(' ');// łączy ele. tablicy w tekst
+  // console.log(allTags);
+  /* [NEW] create variable for all links HTML code */
+  let allTagsHTML = '';
+
+
+  /* [NEW] START LOOP: for each tag in allTags */
+  for (let tag in allTags) {
+    const tagLinkHTML =
+      '<li><a href="#tag-' +
+      tag +
+      '" class="' +
+      calculateTagClass(allTags[tag], tagsParams) +
+      '">' +
+      tag +
+      '</a></li>';
+    allTagsHTML += tagLinkHTML;
+    console.log('taglinkHTML', tagLinkHTML);
+    /*[NEW] END LOOP: for each tag in allTags*/
+  }
+
+
+  /*[NEW] add html from allTagsHTML to tagList */
+  tagList.innerHTML = allTagsHTML;
+}
 generateTags();
 
 
@@ -200,6 +272,7 @@ function addClickListenersToTags() {
   }
 }
 
+
 addClickListenersToTags();
 
 function generatorAuthors() {
@@ -210,7 +283,7 @@ function generatorAuthors() {
   for (let article of articles) {
     /* find authors wrapper */
     const authorsWrapper = article.querySelector(optAuthorsSelector);
-    console.log(authorsWrapper);
+    // console.log(authorsWrapper);
 
     /* make html variable with empty string */
     let html = '';
